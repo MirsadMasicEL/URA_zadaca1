019104859e473df703ac2cc2fe8cd6e6371fe63f
SmajO4 <smajil.music@fet.ba>
2025-05-06 19:19:12 +0200

    Create zadatak5.cpp

---
+++ zadatak5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+int prod_even(int n){
+  if(n <= 3)
+    return 2;
+
+  if(n & 1)
+    return (n-1) * prod_even(n-3);
+  else
+    return n * prod_even(n-2);
+}
+
+// poprilicno ne tako tolko drukcija varijanta
+
+int prod_even_2(int n) {
+  if(n < 2) return 1;
+
+  if(n & 1)
+    return prod_even_2(n-1);
+  else
+    return n * prod_even_2(n - 2);
+}
+
+
+using namespace std;
+
+int main() {
+  cout << prod_even(8) << endl;
+  cout << prod_even_2(8) << endl;
+
+  return 0;
+}
