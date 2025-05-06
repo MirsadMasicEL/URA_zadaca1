9a01417e806691dee2b37da222e9ed6d8b5dd932
SmajO4 <smajil.music@fet.ba>
2025-05-06 19:13:26 +0200

    Create Zadatak4.cpp

---
+++ Zadatak4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int sum(const int* array, int n){
+  if(n < 1) return 0;
+  return array[0] + sum(array + 1, n - 1);
+}
+
+using namespace std;
+
+int main(){
+  int niz[4] = {4, 7, 1, 3};
+  cout << sum(niz, 4) << endl;
+}
