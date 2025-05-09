b57106613afcf82280cd577f7adfee085740c8c0
MirsadMasicEL <168371752+MirsadMasicEL@users.noreply.github.com>
2025-05-09 14:48:57 +0200

    n-ti commit

---
+++ zadatak9/main1.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+
+template <typename F> auto benchmark(std::string desc, F &&function) {
+  auto now = std::chrono::steady_clock::now();
+  function();
+  auto elapsed = std::chrono::steady_clock::now() - now;
+  std::cout << "Finished benchmark: " << desc << std::endl;
+  std::cout << "Duration: " << elapsed.count() / 1000 << "us" << std::endl;
+}
+
+int main() {
+  std::vector<std::string> vec;
+  std::string str;
+  std::ifstream input{"shakespeare/shakespeare100k.txt"};
+  if(!input) {
+    std::cout << "Greska pri ucitavanju file-a\n";
+  }
+  std::cout << "Pocetak ucitavanja." << std::endl;
+  benchmark("Loading the file", [&]() {
+    while (input >> str)
+      vec.push_back(str);
+  });
+  std::cout << "Kraj ucitavanja." << std::endl;
+  input.close();
+  std::cout << "Pocetak sortiranja." << std::endl;
+  benchmark("Sorting the file", [&]() {
+    std::sort(vec.begin(), vec.end());
+  });
+  std::cout << "Kraj sortiranja." << std::endl;
+  return 0;
+}
---
+++ zadatak9/main2.cpp
@@ -0,0 +1,41 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <typename T, typename It>
+It upper_bound(It begin, It end, const T &element) {
+  It returnVal = end;
+  while (begin < end) {
+    int difference = (end - begin) / 2;
+    It current = begin + difference;
+    if (element >= *current) {
+      begin = current + 1;
+    } else {
+      returnVal = current;
+      end = current;
+    }
+  }
+  return returnVal;
+}
+
+template <typename F> auto benchmark(std::string desc, F &&function) {
+  auto now = std::chrono::steady_clock::now();
+  function();
+  auto elapsed = std::chrono::steady_clock::now() - now;
+  std::cout << "Finished benchmark: " << desc << std::endl;
+  std::cout << "Duration: " << elapsed.count() / 1000 << "us" << std::endl;
+}
+
+int main() {
+  std::ifstream input{"shakespeare/shakespeare50k.txt"};
+  std::vector<std::string> vec;
+  std::string str;
+  benchmark("Ucitavanje i sortiranje.", [&]() {
+    while (input >> str) {
+      auto it = upper_bound(vec.begin(), vec.end(), str);
+      vec.insert(it, str);
+    }
+  });
+}
